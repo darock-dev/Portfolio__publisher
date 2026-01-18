d73e351a1ac0ba1a7ba6793fb4ae1f6e2679e064
Darock <heetask@gmail.com>
2026-01-18 17:42:38 +0900

    style: 헤더 모바일버전 스타일링 적용

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -5,39 +5,35 @@ export default function Header() {
   return (
     <header className={styles.header}>
       <div className={styles.inner}>
-        <div className={styles.left}>
-          <div className={styles.logo}>
-            <a href="#">
-              <img src={logo} alt="로고" />
-            </a>
-          </div>
+        <div className={styles.logo}>
+          <a href="#">
+            <img src={logo} alt="로고" />
+          </a>
         </div>
-        <div className={styles.right}>
-          <nav className={styles.nav}>
-            <ul>
-              <li>
-                <a href="#animation">Animation</a>
-              </li>
-              <li>
-                <a href="#accordion-menu">Accordion Menu</a>
-              </li>
-              <li>
-                <a href="#form">Form</a>
-              </li>
-              <li>
-                <a href="#slide">Slide</a>
-              </li>
-              <li>
-                <a href="#contact">Contact</a>
-              </li>
-            </ul>
-          </nav>
-          <button className={styles.menuToggle} type="button">
-            <span></span>
-            <span></span>
-            <span></span>
-          </button>
-        </div>
+        <nav className={`${styles.nav}`}>
+          <ul>
+            <li>
+              <a href="#animation">Animation</a>
+            </li>
+            <li>
+              <a href="#accordion-menu">Accordion Menu</a>
+            </li>
+            <li>
+              <a href="#form">Form</a>
+            </li>
+            <li>
+              <a href="#slide">Slide</a>
+            </li>
+            <li>
+              <a href="#contact">Contact</a>
+            </li>
+          </ul>
+        </nav>
+        <button className={`${styles.menuToggle}`} type="button">
+          <span></span>
+          <span></span>
+          <span></span>
+        </button>
       </div>
     </header>
   );
