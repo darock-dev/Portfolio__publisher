ef49a2f396afcabb0e9876058763fb996d1dc811
Darock <heetask@gmail.com>
2026-01-18 15:42:31 +0900

    refactor: 스타일 디렉토리 구조 변경 및 reset/variables 추가

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
-import './App.scss'
-import Layout from './components/Layout/Layout'
+import "./styles/App.scss";
+import Layout from "./components/Layout/Layout";
 
 function App() {
   return (
     <>
       <Layout></Layout>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
