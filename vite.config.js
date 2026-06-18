33897218ef3939c34f1a0386099166234030d05f
Dova <56466589+darock-dev@users.noreply.github.com>
2026-06-18 07:13:39 +0000

    build: 소스맵 생성 설정 활성화

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,8 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  build: {
+    sourcemap: true // 소스맵 생성 활성화
+  },
   base: "https://darock-dev.github.io/Portfolio__publisher/",
 });
